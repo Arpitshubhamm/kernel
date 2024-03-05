c65fe4bec3ce201f84f76d4ebbd3af0b05ca24df
Arpitshubhamm <157899697+Arpitshubhamm@users.noreply.github.com>
2024-03-05 15:38:13 +0530

    Create sync.sh

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Make sure to always install these packages to avoid build error abour rsycn, bison and flex
+sudo apt-get install bison flex rsycn -y
+
+# Make sure repo is installed
+mkdir -p ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+sudo ln -sf ~/bin/repo /usr/bin/repo
+
+# Create directory for repo sync
+mkdir -p android-kernel
+cd android-kernel
+
+# Disable Color UI
+git config --global color.ui false
+
+# Initialized manifest
+repo init -u https://github.com/xiaomi-mt6789-devs/kernel_manifest-yunluo -b udc
+
+# Sync all repository inside the manifest
+repo sync
